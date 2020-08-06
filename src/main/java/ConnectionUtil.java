4d7ef0d782d24b615608c22f210fe65958e418ed
AlexStasiuk <sov853972@gmail.com>
2020-08-06 17:00:04 +0300

    add connectionUtil

---
+++ src/main/java/ConnectionUtil.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/ishop";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "198823";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't find a class");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't connect to DB");
+        }
+    }
+}
