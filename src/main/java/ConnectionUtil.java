23ba5a313e54b5f52b81144eec96cf0389dc6591
AlexStasiuk <sov853972@gmail.com>
2020-08-06 17:56:16 +0300

    fix bug

--- src/main/java/ConnectionUtil.java
+++ src/main/java/ConnectionUtil.java
@@ -2,22 +2,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class ConnectionUtil {
+
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/ishop";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "198823";
 
     public static Connection getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("org.gjt.mm.mysql.Driver");
             return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Can't find a class");
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("Can't connect to DB");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("can't find jdbc class");
         }
     }
 }
+
+
