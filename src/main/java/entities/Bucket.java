14c6d496c46cc527dc420f64ed6d9dc52f37702a
AlexStasiuk <sov853972@gmail.com>
2020-08-06 16:40:10 +0300

    add entities

---
+++ src/main/java/entities/Bucket.java
@@ -0,0 +1,53 @@
+package entities;
+
+
+import java.util.Date;
+
+public class Bucket {
+    private int id;
+    private int user_id;
+    private int product_id;
+    private Date dateTime;
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", product_id=" + product_id +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,60 @@
+package entities;
+
+public class User {
+    private int id;
+    private String email;
+    private String name;
+    private String surname;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
