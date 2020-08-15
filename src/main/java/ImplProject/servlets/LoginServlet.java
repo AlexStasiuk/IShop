2da93be23e1e310d3d7ecc7a0c5c71a94f89ad9b
AlexStasiuk <sov853972@gmail.com>
2020-08-15 22:02:38 +0300

    add bootstrap js styles fix servlets

--- src/main/java/ImplProject/servlets/LoginServlet.java
+++ src/main/java/ImplProject/servlets/LoginServlet.java
@@ -17,30 +17,46 @@ public class LoginServlet extends HttpServlet {
 
     private UserService userService = UserService.getInstance();
 
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
-    }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+            throws IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        if (!ObjectUtils.allNotNull(email, password)) {
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+        if (email == null || password == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
         Optional<User> user = userService.getByEmail(email);
-
-        if (user.isPresent() && user.get().getPassword().equalsIgnoreCase(password)) {
-            request.setAttribute("userEmail", email);
-            request.getRequestDispatcher("cabinet.jsp").forward(request, response);
+        // Todo move to service
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            response.setStatus(HttpServletResponse.SC_OK);
             return;
         }
-        // Todo Redirect?
-        request.getRequestDispatcher("login.jsp").forward(request, response);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
     }
+
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//
+//        if (!ObjectUtils.allNotNull(email, password)) {
+//            request.getRequestDispatcher("login.jsp").forward(request, response);
+//            return;
+//        }
+//
+//        Optional<User> user = userService.getByEmail(email);
+//
+//        if (user.isPresent() && user.get().getPassword().equalsIgnoreCase(password)) {
+//            request.setAttribute("userEmail", email);
+//            request.getRequestDispatcher("cabinet.jsp").forward(request, response);
+//            return;
+//        }
+//        // Todo Redirect?
+//        request.getRequestDispatcher("login.jsp").forward(request, response);
+//    }
 }
